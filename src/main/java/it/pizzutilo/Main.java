ea4bfa248a1d51f9eb2916aebdcc7871aa9c0b7d
tommipiz <tommaso.pizzutilo@gmail.com>
2024-10-09 09:16:13 +0200

    Server completed

--- src/main/java/it/pizzutilo/Main.java
+++ src/main/java/it/pizzutilo/Main.java
@@ -1,7 +1,28 @@
 package it.pizzutilo;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(5637);
+        Socket s = ss.accept();
+        System.out.println("Qualcuno si è collegato");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        String stringRicevuta = in.readLine();
+        System.out.println("La stringa ricevuta: " + stringRicevuta);
+
+        out.writeBytes("Server" + "\n");
+
+        s.close();
+        ss.close();
+
     }
 }
